a1cb514979a7aae0ae0e678959fe5274bf5242e0
audtnnn <kaudtn12@gmail.com>
2024-02-27 17:49:29 +0900

    first commit

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,34 @@
+<template>
+  <footer>
+    <button type="button" @click="finishRemove">완료일정 삭제</button>
+    <button type="button" @click="allRemove">전체 삭제</button>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "TodoFooter",
+  methods: {
+    finishRemove(){
+      this.$store.commit("finish__Remove")
+    },
+    allRemove(){
+      this.$store.commit("all__Remove")
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  footer {
+    padding: 30px;
+    text-align: center;
+    button {
+      background: #000;
+      color: #fff;
+      border-radius: 5px;
+      padding: 10px;
+      margin: 0 10px;
+    }
+  }
+</style>
---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <h1>일정관리앱 {{completedCount}}/{{totalCount}}건 처리</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "TodoHeader",
+  computed: {
+    completedCount() {
+      let todos = this.$store.getters.fnGetTodos
+      return todos.filter(item=>item.modify==true).length
+    },
+    totalCount() {
+      let todos = this.$store.getters.fnGetTodos
+      return todos.length
+    }
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  background: #22b8cf;
+  color: #fff;
+  font-size: 24px;
+  text-align: center;
+  padding: 20px;
+}
+</style>
